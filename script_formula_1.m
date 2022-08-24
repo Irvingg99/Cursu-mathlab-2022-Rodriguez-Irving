746a7277d6015c48c8454fa0d9cfe5d9b81a1928
Irvingg99 <111807424+Irvingg99@users.noreply.github.com>
2022-08-24 11:15:36 -0500

    Add files via upload

---
+++ script_formula_1.m
@@ -0,0 +1,10 @@
+disp (["Para resolver por la formula general ingresar los valores de ¨b¨ y ¨c¨, el valor de ¨a¨ sera igual a 1"])
+
+b = input ("Dame el valor para b ");
+c = input ("Dame el valor para c "); 
+
+x1 = (-b+sqrt((b^2)-4*1*c))/2*1;
+x2 = (-b-sqrt((b^2)-4*1*c))/2*1;
+
+disp (["El valor de x1 es",x1]) 
+disp (["El valor de x2 es",x2])
